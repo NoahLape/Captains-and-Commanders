683482be4a8f77a2d6bd86eb31b75a06ee0623fa
Noah.D.Lape <noah.d.lape@gmail.com>
2020-07-07 15:57:43 -0400

    Add files via upload

---
+++ android/src/com/candc/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.candc;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.candc.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
---
+++ desktop/src/com/candc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.candc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.candc.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
